08156e4335ef279706d21c2dd6495b7cce1cfa9d
Duane Jeffers <duane@duanejeffers.com>
2014-12-02 15:40:44 -0600

    Adding Schema

---
+++ data/schema/0.1.sql
@@ -0,0 +1,38 @@
+
+DROP TABLE IF EXISTS "entries";
+CREATE TABLE "entries" (
+  "userid" integer NOT NULL,
+  "tweetid" integer NOT NULL,
+  "added" integer NOT NULL,
+  FOREIGN KEY ("userid") REFERENCES "user" ("userid"),
+  FOREIGN KEY ("tweetid") REFERENCES "tracktweet" ("id")
+);
+
+
+DROP TABLE IF EXISTS "settings";
+CREATE TABLE "settings" (
+  "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+  "name" text NOT NULL,
+  "value" text NOT NULL
+);
+
+
+DROP TABLE IF EXISTS "tracktweet";
+CREATE TABLE "tracktweet" (
+  "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+  "tweetid" integer NOT NULL,
+  "lasttracked" integer NOT NULL
+);
+
+
+DROP TABLE IF EXISTS "user";
+CREATE TABLE "user" (
+  "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+  "twitterid" integer NOT NULL,
+  "username" text NOT NULL,
+  "user_object" text NOT NULL,
+  "follower" integer NOT NULL DEFAULT '0',
+  "winner" integer NOT NULL DEFAULT '0',
+  "exclude" integer NOT NULL DEFAULT '0',
+  "added" integer NOT NULL
+);
