52d8ef5eea08f0df2f158cfac4a824a5b9f4f303
Duane Jeffers <duane@duanejeffers.com>
2014-12-03 00:43:55 -0600

    Added group for /track, cleaned up settings array/object, formatted for speed

--- public/index.php
+++ public/index.php
@@ -30,39 +30,48 @@
 			  ->appendStyle("body { padding-top: 60px; }")
 			  ->appendStylesheet('//cdnjs.cloudflare.com/ajax/libs/jasny-bootstrap/3.1.3/css/jasny-bootstrap.min.css');
 
-	// Setup Codebird Twitter stuff.
-	$consumer_key = \ORM::for_table('settings')->where('name', 'twitter_consumer_key')->find_one();
-	$consumer_secret = \ORM::for_table('settings')->where('name', 'twitter_consumer_secret')->find_one();
+	$settings = \ORM::for_table('settings')->select_many('name', 'value')->find_array();
 
-	\Codebird\Codebird::setConsumerKey($consumer_key->value, $consumer_secret->value);
+	$app->app_settings = (object) array_column($settings, 'value', 'name');
+
+	// Setup Codebird Twitter stuff.
+	\Codebird\Codebird::setConsumerKey($app->app_settings->twitter_consumer_key, $app->app_settings->twitter_consumer_secret);
 
 	$app->container->singleton('twitter', function() {
 		return \Codebird\Codebird::getInstance();
 	});
 
-	$user_token = \ORM::for_table('settings')->where('name', 'twitter_access_token')->find_one();
-	$user_secret = \ORM::for_table('settings')->where('name', 'twitter_access_token_secret')->find_one();
-
-	$app->twitter->setToken($user_token->value, $user_secret->value);
+	$app->twitter->setToken($app->app_settings->twitter_access_token, $app->app_settings->twitter_access_token_secret);
 
 	$app->get('/', function() use($app) {
-		$last_run = \ORM::for_table('settings')->select('value')->where('name', 'last_run')->find_one();
-		$last_run_date = date('l, F j, Y h:i:s', $last_run->value);
+		$last_run_date = date('l, F j, Y h:i:s', $app->app_settings->last_run);
 
 		$total_entries = \ORM::for_table('entries')->count();
-
 		$total_users = \ORM::for_table('user')->where('exclude', 0)->count();
-
 		$total_followers = \ORM::for_table('user')->where('exclude', 0)->where('follower', 1)->count();
 
 		$not_following = \ORM::for_table('user')->where('exclude', 0)->where('follower', 0)->find_many();
 		$not_following_count = \ORM::for_table('user')->where('exclude', 0)->where('follower', 0)->count();
+
 		$users_unfollowing = array();
 		foreach($not_following as $user) {
-			$users_unfollowing[] = array(
-				'username' => $user->username,
-				'entry_count' => \ORM::for_table('entries')->where('userid', $user->id)->count()
-			);
+			$users_unfollowing[] = sprintf('<tr><td>%s</td><td>%s</td></tr>', 
+										   $user->username, 
+										   \ORM::for_table('entries')->where('userid', $user->id)->count());
+		}
+
+		$stats = \ORM::for_table('tracktweet')->table_alias('tt')
+											  ->select('tt.tweetid')
+											  ->select_expr('COUNT(e.tweetid)', 'rt_count')
+											  ->left_outer_join('entries', array('tt.id', '=', 'e.tweetid'), 'e')
+											  ->group_by("e.tweetid")
+											  ->find_many();
+		$tweet_stats = array();
+		foreach($stats as $stat) {
+			$tweet_stats[] = sprintf('<tr><td><a href="http://twitter.com/%s/status/%s">%s</a></td><td>%s</td></tr>', $app->app_settings->twitter_username,
+																													  $stat->tweetid,
+																													  $stat->tweetid,
+																													  $stat->rt_count);
 		}
 
 		$app->render('index.php', array(
@@ -70,17 +79,33 @@
 			'total_entries' => $total_entries,
 			'total_users' => $total_users,
 			'total_followers' => $total_followers,
-			'users_unfollowing' => $users_unfollowing,
-			'unfollowing_count' => $not_following_count
+			'users_unfollowing' => implode($users_unfollowing),
+			'unfollowing_count' => $not_following_count,
+			'tweet_stats' => implode($tweet_stats)
 		));
 	});
 
-	$app->map('/track', function() use($app) {
-		if($app->request->isPost()) { // We need to process the URL and get the tweet id from it.
+	$app->group('/track', function() use($app) {
+		$app->get('/', function() use($app) {
+			// Grab Current Statuses
+			$track = \ORM::for_table('tracktweet')->table_alias('tt')
+												  ->select_many('tt.tweetid', 'tt.lasttracked')
+												  ->select_expr('COUNT(e.tweetid)', 'rt_count')
+												  ->left_outer_join('entries', array('tt.id', '=', 'e.tweetid'), 'e')
+												  ->group_by("e.tweetid")
+												  ->find_many();
+
+			$active_track = array();
+			foreach($track as $t) {
+				//$active_track[] = sprintf('<tr><td><a href="%s">%s</a></td><td>%s</td><td><a href="%s">Stop Tracking</a></td></tr>');	
+			}
+			$app->render('track.php', array());
+		});
+
+		$app->post('/add', function() use($app) {
 			$url = $app->request->post('twitterurl');
 			$pieces = parse_url($url);
-			$parts = explode('/', $pieces['path']);
-			$id = array_pop($parts);
+			$id = array_pop(explode('/', $pieces['path']));
 
 			$tweetcount = \ORM::for_table('tracktweet')->where('tweetid', $id)->count();
 			if($tweetcount === 0) {
@@ -88,16 +113,15 @@
 				$track = \ORM::for_table('tracktweet')->create();
 				$track->tweetid = $id;
 				$track->lasttracked = time();
-				$track->save();
+					$track->save();
 				$app->flash('success', 'Added Status to Tracker.');
 			} else {
 				$app->flash('danger', 'Status is already being tracked.');
 			}
 
-			$app->redirect('/');
-		}
-		$app->render('track.php', array());
-	})->via('GET', 'POST')->name('track');
+			$app->redirect('/track');
+		});
+	});
 
 	$app->post('/search', function() use($app) {
 		$username = $app->request->post('username');
