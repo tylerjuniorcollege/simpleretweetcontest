6c95b027fe11a444ce16cd24ae60c9a5366d2be4
Duane Jeffers <duane@duanejeffers.com>
2014-12-03 17:02:05 -0600

    Updated Tweet Stats for the main page.

--- app/templates/index.php
+++ app/templates/index.php
@@ -4,7 +4,7 @@
 	</div>
 </div>
 <div class="row">
-	<div class="col-md-4"> 
+	<div class="col-md-3"> 
 		<div class="panel panel-success">
 			<div class="panel-heading">
 				<h4 class="panel-title">Total Number of Retweets</h4>
@@ -14,7 +14,7 @@
 			</div>
 		</div>
 	</div>
-	<div class="col-md-4"> 
+	<div class="col-md-3"> 
 		<div class="panel panel-info">
 			<div class="panel-heading">
 				<h4 class="panel-title">Total Number of Users</h4>
@@ -24,7 +24,7 @@
 			</div>
 		</div>
 	</div>
-	<div class="col-md-4"> 
+	<div class="col-md-3"> 
 		<div class="panel panel-warning">
 			<div class="panel-heading">
 				<h4 class="panel-title">Total Number of Followers</h4>
@@ -34,11 +34,46 @@
 			</div>
 		</div>
 	</div>
+	<div class="col-md-3"> 
+		<div class="panel panel-primary">
+			<div class="panel-heading">
+				<h4 class="panel-title">Average (Mean) Retweets</h4>
+			</div>
+			<div class="panel-body">
+				<h1 id="totalfollowers"><?= $data['average_retweets']; ?></h1>
+			</div>
+		</div>
+	</div>
 </div>
 <div class="row">
 	<div class="col-md-12">
-		<h3 class="page-header"><?= $data['unfollowing_count'] ?> Users not following ...</h3>
-		<table class="table table-condensed table-hover">
+		<h3 class="page-header"><a data-toggle="collapse" data-target="#tweetstats" href="#tweetstats" aria-expanded="false" aria-controls="tweetstats">Tweet Stats</a> <small><?= $data['tweets_tracking']; ?> Total Tweets being Tracked</small> 
+								<a data-toggle="collapse" data-target="#tweetstats" href="#tweetstats" aria-expanded="false" aria-controls="tweetstats" class="pull-right"><span class="glyphicon glyphicon-chevron-down"></span></a></h3>
+		<div class="clearfix"></div>
+		<table class="table table-condensed table-hover collapse in" id="tweetstats">
+			<colgroup>
+				<col class="col-md-4">
+				<col class="col-md-4">
+				<col class="col-md-4">
+			</colgroup>
+			<thead>
+				<tr>
+					<th>Tweet ID</th>
+					<th># of Retweets</th>
+					<th>View</th>
+				</tr>
+			</thead>
+			<tbody>
+			<?=$data['tweet_stats'] ?>
+			</tbody>
+		</table>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-12">
+		<h3 class="page-header"><a data-toggle="collapse" data-target="#notfollowing" href="#notfollowing" aria-expanded="true" aria-controls="notfollowing"><?= $data['unfollowing_count'] ?> Users not following ...</a>
+								<a data-toggle="collapse" data-target="#notfollowing" href="#notfollowing" aria-expanded="true" aria-controls="notfollowing" class="pull-right"><span class="glyphicon glyphicon-chevron-down"></span></a></h3>
+		<table class="table table-condensed table-hover collapse" id="notfollowing">
 			<thead>
 				<tr>
 					<th>Username</th>
@@ -47,22 +82,6 @@
 			</thead>
 			<tbody>
 			<?=($data['users_unfollowing']) ?>
-			</tbody>
-		</table>
-	</div>
-</div>
-<div class="row">
-	<div class="col-md-12">
-		<h3 class="page-header">Tweet Stats</h3>
-		<table class="table table-condensed table-hover">
-			<thead>
-				<tr>
-					<th>Tweet ID</th>
-					<th># of Retweets</th>
-				</tr>
-			</thead>
-			<tbody>
-			<?=$data['tweet_stats'] ?>
 			</tbody>
 		</table>
 	</div>
