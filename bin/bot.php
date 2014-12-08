4204d7c1843d9e1f63ce0da8ac394c943cf1b06e
Duane Jeffers <duane@duanejeffers.com>
2014-12-07 22:10:35 -0600

    Cleaner script interface, starting to build for special conditions

--- bin/bot.php
+++ bin/bot.php
@@ -3,12 +3,44 @@
 	
 	require_once('../vendor/autoload.php');
 
+	function wait_until($timestamp, $skip_sleep = FALSE) {
+		$now = new DateTime();
+		$wait = new DateTime();
+		$wait->setTimestamp($timestamp);
+
+		$interval = $wait->diff($now);
+
+		if($interval->i > 1 && $skip_sleep == TRUE) { // This is if the wait is too long.
+			return TRUE; // Continue past the loop.
+		}
+
+		sleep(($wait->getTimestamp() - $now->getTimestamp()));
+
+		return FALSE;
+	}
+
+	function print_rate_limit($opt, $limit, $remaining, $reset) {
+		global $cli;
+
+		$now = new DateTime();
+		$future = new DateTime();
+		$future->setTimestamp($reset);
+
+		$interval = $future->diff($now);
+
+		if($cli->opt($opt)) {
+			$cli->print_line(sprintf('Rate Limit Remaning (%s/%s) Time Remaning Til Reset: %s', $remaining, $limit, $interval->format("%i minutes, %s seconds")));
+		}
+	}
+
 	$cli = new Cli(array(
 		'tracktweet:' => 'This lets you add a tweet to the tracker.',
 		'processtweets' => 'Processes all retweets of tracked tweets',
 		'processfollowers' => 'Checks to see if processed users are followers',
-		'findwinner' => 'This will help specify a winner that is following the main account. (Will not choose any excluded users)',
-		'excludewinners' => 'This switch will allow you to exclude all winnners from the picker.'
+		'processstats:' => 'Processes stats for the specified Campaign ID',
+		'showratelimit' => 'Show the current ratelimit',
+		'showratelimits' => 'Application Rate Limit'
+
 	));
 
 	\ORM::configure('sqlite:../data/database.db');
@@ -27,7 +59,11 @@
 	$twitter->setToken($user_token->value, $user_secret->value);
 
 	// Adding in time for timestamps.
-	$time = time();
+	if($cli->opt('showratelimits')) {
+		$rts = $twitter->application_rateLimitStatus();
+		$cli->print_dump($rts);
+		die();
+	}
 
 	if($cli->opt('tracktweet')) {
 		$tweetcount = \ORM::for_table('tracktweet')->where('tweetid', $cli->opt('tracktweet', TRUE))->count();
@@ -36,7 +72,7 @@
 			// Add it to the database.
 			$track = \ORM::for_table('tracktweet')->create();
 			$track->tweetid = $cli->opt('tracktweet', TRUE);
-			$track->lasttracked = $time;
+			$track->lasttracked = time();
 			$track->save();
 			$cli->print_line('Added tweet to tracker.');
 		} else {
@@ -51,17 +87,35 @@
 
 		//var_dump($tweets);
 		foreach($tweets as $tweet) {
-			/* $retweets = $twitter->statuses_retweets_ID(sprintf('id=%s&limit=100', $tweet->tweetid), true);
-			$cli->print_line(sprintf('Processing Retweets for %s Total# %s', $tweet->tweetid, count((array) $retweets))); */
-			// Create a loop to check if there are more than 100 retweets on a single status.
 			$retweeters = array();
-			$req_str = sprintf('id=%s', $tweet->tweetid);
+			$req_str = array('id' => $tweet->tweetid);
 			$cursor = NULL;
 			do {
 				if(!is_null($cursor)) {
-					$req_str .= sprintf('&cursor=%s', $cursor);
+					$req_str['cursor'] = $cursor;
 				}
-				$retweet_gather = $twitter->statuses_retweeters_ids($req_str, true);
+				do {
+					$retweet_gather = $twitter->statuses_retweeters_ids($req_str);
+
+					if(!is_array($retweet_gather->rate)) {
+						// Pause and try again
+						$retry = TRUE;
+						continue;
+					} else {
+						$retry = FALSE;
+					}
+					print_rate_limit('showratelimit', $retweet_gather->rate['limit'], $retweet_gather->rate['remaining'], $retweet_gather->rate['reset']);
+
+					if($retweet_gather->rate['remaining'] < 2) {
+						// Wait 
+						$continue = wait_until($retweet_gather->rate['reset'], TRUE);
+						if($continue == TRUE) {
+							$cli->print_line('Rate Limit Reached Please Restart later.');
+							continue 4;
+						}
+					}
+				} while((!is_object($retweet_gather)) || ($retry == TRUE));
+
 				$retweeters = array_merge($retweeters, $retweet_gather->ids);
 
 				if($retweet_gather->next_cursor !== 0) {
@@ -74,50 +128,57 @@
 
 			foreach($retweeters as $rtid) {
 				// First, search for the user in the user table. If they don't exist, then we need to create a row for them.
-				$user_search = \ORM::for_table('user')->where('twitterid', $rtid)->find_one();
+				$user = \ORM::for_table('user')->where('twitterid', $rtid)->find_one();
 
-				if($user_search === false) {
+				if($user === false) {
 					// Grab the information from the API.
-					$user_info = $twitter->users_show(sprintf('user_id=%s', $rtid), true);
+					do {
+						$user_info = $twitter->users_show(http_build_query(array('user_id' => $rtid)));
+					if(!is_array($user_info->rate)) {
+						// Pause and try again
+						$retry = TRUE;
+					}
+					print_rate_limit('showratelimit', $user_info->rate['limit'], $user_info->rate['remaining'], $user_info->rate['reset']);
+
+					if($user_info->rate['remaining'] > 2) {
+						wait_until($user_info->rate['reset']);
+					}
+				} while((!is_object($user_info)) || ($retry == TRUE));
 					// Now we need to create the user object.
-					$user_db = \ORM::for_table('user')->create();
-					$user_db->twitterid = $rtid;
-					$user_db->username = $user_info->screen_name;
-					$user_db->user_object = serialize($user_info);
-					$user_db->added = $time;
-					$user_db->save();
-					$user = $user_db;
-					unset($user_db);
+
+					if(!isset($user_info->screen_name)) {
+						$cli->print_dump($user_info);
+						die();
+					}
+					$user = \ORM::for_table('user')->create();
+					$user->twitterid = $rtid;
+					$user->username = (isset($user_info->screen_name) ? $user_info->screen_name : '');
+					$user->user_object = serialize($user_info);
+					$user->added = time();
+					$user->save();
 					$cli->print_line(sprintf('Added User (%s) to the database', $user->username));
 					sleep(5);
-				} else {
-					$user = $user_search;
-					unset($user_search);
 				}
 
 				// See if the user is already entered for this tweet.
-				$entry_search = \ORM::for_table('entries')->where(array(
+				$entry = \ORM::for_table('entries')->where(array(
 					'userid' => $user->id,
 					'tweetid' => $tweet->id 
 				))->find_one();
 
-				if($entry_search === false) {
+				if($entry === false) {
 					$entry = \ORM::for_table('entries')->create();
 					$entry->userid = $user->id;
 					$entry->tweetid = $tweet->id;
-					$entry->added = $time;
+					$entry->added = time();
 					$entry->save();
 					$cli->print_line(sprintf('Added Entry for User (%s) for Tweet ID#%s', $user->username, $tweet->tweetid));
-					unset($entry);
 					$retweetcount++;
 				}
-
-				// Skip already entered user.
-				unset($entry_search);
 			}
 
 			// Update time stamp on Tweet tracker.
-			$tweet->lasttracked = $time;
+			$tweet->lasttracked = time();
 			$tweet->save();
 		}
 	}
@@ -130,7 +191,18 @@
 
 		// Split array in to 100s.
 		foreach(array_chunk($user_arr, 100) as $user_chunk) {
-			$following = $twitter->friendships_lookup(sprintf('screen_name=%s', implode(',', $user_chunk)));
+			do {
+				$following = $twitter->friendships_lookup(http_build_query(array('screen_name' => implode(',', $user_chunk))));
+					if(!is_array($following->rate)) {
+						// Pause and try again
+						$retry = TRUE;
+					}
+					print_rate_limit('showratelimit', $following->rate['limit'], $following->rate['remaining'], $following->rate['reset']);
+
+					if($following->rate['remaining'] > 2) {
+						wait_until($following->rate['reset']);
+					}
+			} while((!is_object($following)) || ($retry == TRUE));
 			foreach((array) $following as $id => $relationship) {
 				if(!is_object($relationship)) {
 					continue; // Skip the processing of these items.
@@ -159,5 +231,5 @@
 
 	$lastrun = \ORM::for_table('settings')->where('name', 'last_run')->find_one();
 
-	$lastrun->value = $time;
+	$lastrun->value = time();
 	$lastrun->save();
